df3e9b807664107c767a07c43a7e4d61aaac3e38
Aleksa Trampa <alektramp@gmail.com>
2023-03-28 21:58:08 +0200

    Created enemy script and take damage method

---
+++ Assets/_Project/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.digitalmind.towertest
+{
+    public class Enemy : MonoBehaviour
+    {
+        public float maxHealth;
+        
+        private float _currentHealth;
+
+        public void TakeDamage(float amount)
+        {
+            CurrentHealth -= amount;
+            Debug.Log($"Enemy took : {amount} damage");
+        }
+        
+        
+        
+        
+        public float CurrentHealth
+        {
+            get => _currentHealth;
+            set => _currentHealth = Mathf.Clamp(_currentHealth - value, 0, maxHealth);
+        }
+    }
+}
--- Assets/_Project/Scripts/Projectile.cs
+++ Assets/_Project/Scripts/Projectile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,23 @@ namespace com.digitalmind.towertest
     public class Projectile : MonoBehaviour
     {
         public float speed;
+        public float damage = 1f;
         void Update()
         {
             transform.Translate(transform.right * (speed * Time.deltaTime));
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                DamageEnemy(other.GetComponent<Enemy>());
+            }
+        }
+
+        private void DamageEnemy(Enemy enemy)
+        {
+            enemy.TakeDamage(damage);
         }
     }
 }
