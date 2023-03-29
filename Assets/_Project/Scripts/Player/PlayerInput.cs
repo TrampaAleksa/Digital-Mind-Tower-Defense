8448e544c0852ced222d6266c8a8fc32e09df098
Aleksa Trampa <alektramp@gmail.com>
2023-03-29 20:50:29 +0200

    Created Player and Enemy folders

---
+++ Assets/_Project/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.digitalmind.towertest
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        
+    }
+}
