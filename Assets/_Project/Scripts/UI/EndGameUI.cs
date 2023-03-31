546872cc987da2b906bec4c1d570ff7af0d6e49c
Aleksa Trampa <alektramp@gmail.com>
2023-03-31 16:58:16 +0200

    restarting and returning to menu unpauses game

--- Assets/_Project/Scripts/UI/EndGameUI.cs
+++ Assets/_Project/Scripts/UI/EndGameUI.cs
@@ -10,21 +10,24 @@ public class EndGameUI : MonoBehaviour
     public void DisplayEndGamePanel()
     {
         PauseGameHandler.Instance.SetIsPaused(true); //TODO - Ui shouldn't handle pause logic
+        DisplayScore();
         gameObject.SetActive(true);
     }
     
     public void DisplayScore()
     {
-        scoreDisplay.text = ScoreHandler.Instance.CurrentScore.ToString();
+        scoreDisplay.text = ScoreHandler.Instance.CurrentScore.ToString(); //TODO - End game ui should know about score handler
     }
 
     public void BackToMainMenu()
     {
+        PauseGameHandler.Instance.SetIsPaused(false);
         SceneManager.LoadScene("MainMenu");
     }
 
     public void RestartGame()
     {
+        PauseGameHandler.Instance.SetIsPaused(false); 
         SceneManager.LoadScene("GameScene");
     }
 }
