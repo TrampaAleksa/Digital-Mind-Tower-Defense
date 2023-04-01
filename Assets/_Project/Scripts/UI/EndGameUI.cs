9c2983e2188414617cb69d33a03bd50f586f09e8
Aleksa Trampa <alektramp@gmail.com>
2023-04-01 13:30:36 +0200

    finished end game panel logic

--- Assets/_Project/Scripts/UI/EndGameUI.cs
+++ Assets/_Project/Scripts/UI/EndGameUI.cs
@@ -2,20 +2,35 @@ using com.digitalmind.towertest;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+
+//TODO - Refactor everything - leaderboard input logic, pause logic; disable input on pause, ui shouldn't handle pause logic
 public class EndGameUI : MonoBehaviour
 {
     public TextMeshProUGUI scoreDisplay;
     public GameObject gameUIPanel;
 
+    public TMP_InputField nameInput;
+
+    public GameObject leaderboardInputContainer;
+    public GameObject noHighScoreContainer;
+
+    private bool _isHighScore = false;
+    
     public void DisplayEndGamePanel()
     {
-        //TODO - Properly extract pause logic
-        //TODO - Disable input and remove game ui once game is over / paused
-        PauseGameHandler.Instance.SetIsPaused(true); //TODO - Ui shouldn't handle pause logic
+        PauseGameHandler.Instance.SetIsPaused(true);
         DisplayScore();
         gameObject.SetActive(true);
         gameUIPanel.SetActive(false);
+
+        _isHighScore = IsHighScore(ScoreHandler.Instance.CurrentScore);
+        
+        if (_isHighScore)
+            DisplayLeaderboardInputContainer();
+        else DisplayNoHighScoreContainer();
+        
     }
     
     public void DisplayScore()
@@ -25,14 +40,40 @@ public class EndGameUI : MonoBehaviour
 
     public void BackToMainMenu()
     {
+        if (_isHighScore) // TODO - Move logic into leaderboard input class
+            new LeaderboardFileSystem()
+                .TryAddingResult(nameInput.text, ScoreHandler.Instance.CurrentScore);
         PauseGameHandler.Instance.SetIsPaused(false);
         SceneManager.LoadScene("MainMenu");
     }
 
     public void RestartGame()
     {
+        if (_isHighScore)
+            new LeaderboardFileSystem()
+                .TryAddingResult(nameInput.text, ScoreHandler.Instance.CurrentScore);
         gameUIPanel.SetActive(true);
         PauseGameHandler.Instance.SetIsPaused(false); 
         SceneManager.LoadScene("GameScene");
+    }
+
+    private bool IsHighScore(int score) {
+        var results = new LeaderboardFileSystem()
+            .ReadResults();
+
+        foreach(var result in results)
+            if(result.score < score) return true;
+
+        return false;
+    }
+
+    private void DisplayLeaderboardInputContainer() {
+        leaderboardInputContainer.SetActive(true);
+        noHighScoreContainer.SetActive(false);
+    }
+
+    private void DisplayNoHighScoreContainer() {
+        leaderboardInputContainer.SetActive(false);
+        noHighScoreContainer.SetActive(true);
     }
 }
