8d15b0346c50c054ba2252c5622448b203367948
Aleksa Trampa <alektramp@gmail.com>
2023-04-02 17:11:16 +0200

    extracted turret rotation class with target to follow logic

---
+++ Assets/_Project/Scripts/AutomatedTurrets/AutoTurretLockOn.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+namespace com.digitalmind.towertest
+{
+    public class AutoTurretLockOn : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/_Project/Scripts/AutomatedTurrets/AutoTurretRotation.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+namespace com.digitalmind.towertest
+{
+    public class AutoTurretRotation : MonoBehaviour
+    {
+        public Transform rotationObj;
+        
+        public float rotSpeed = 6f;
+        public float lockOnAngle = 2.5f;
+
+        private Transform _initialTarget;
+        private Transform _target;
+
+        private void Start()
+        {
+            _initialTarget = new GameObject("InitialTarget").transform;
+            _initialTarget.position = rotationObj.position;
+            _initialTarget.rotation = rotationObj.rotation;
+        }
+
+        public void RotateToTarget(Transform target)
+        {
+            _target = target;
+            
+            TurretRotation =
+                Quaternion.Slerp(
+                    TurretRotation,
+                    RotationToTarget,
+                    Time.deltaTime * rotSpeed);
+        }
+
+        public void RotateToInitialPosition()
+        {
+            _target = _initialTarget;
+            
+            TurretRotation =
+                Quaternion.Slerp(TurretRotation, _initialTarget.rotation, Time.deltaTime * rotSpeed);
+        }
+
+        
+        public Quaternion TurretRotation { get => rotationObj.rotation; private set => rotationObj.rotation = value; }
+
+        public Vector3 DirectionToTarget => _target.position - rotationObj.position;
+        public Quaternion RotationToTarget => Quaternion.LookRotation(DirectionToTarget);
+        public bool IsLookingAtTarget => Vector3.Angle(rotationObj.forward, DirectionToTarget) < lockOnAngle;
+        
+    }
+}
