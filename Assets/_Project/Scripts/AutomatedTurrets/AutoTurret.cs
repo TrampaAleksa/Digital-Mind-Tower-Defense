1c7c9de9cf77726680682684b61a1fee7c9d5796
Aleksa Trampa <alektramp@gmail.com>
2023-04-02 17:39:01 +0200

    finished extracting auto turret lock on

--- Assets/_Project/Scripts/AutomatedTurrets/AutoTurret.cs
+++ Assets/_Project/Scripts/AutomatedTurrets/AutoTurret.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace com.digitalmind.towertest
 {
@@ -11,16 +10,13 @@ namespace com.digitalmind.towertest
     {
         private AutoTurretGun _turretGun;
         private AutoTurretRotation _turretRotation;
+        private AutoTurretLockOn _turretLockOn;
         
-        public float range = 1.8f;
-
-        private List<Enemy> _enemiesInRange = new List<Enemy>();
-
         private void Start()
         {
-            GetComponent<CapsuleCollider>().radius = range;
             _turretGun = GetComponent<AutoTurretGun>();
             _turretRotation = GetComponent<AutoTurretRotation>();
+            _turretLockOn = GetComponent<AutoTurretLockOn>();
         }
 
         private void Update()
@@ -31,12 +27,6 @@ namespace com.digitalmind.towertest
                 HandleDefaultState();
         }
 
-        private void HandleDefaultState()
-        {
-            _turretRotation.RotateToInitialPosition();
-            _turretGun.TryReloading();
-        }
-
         private void HandleLockOnEnemy()
         {
             _turretRotation.RotateToTarget(LockedOnEnemy.transform);
@@ -44,57 +34,16 @@ namespace com.digitalmind.towertest
             _turretGun.TryShooting(this);
         }
 
-
-        private void OnTriggerEnter(Collider other)
+        private void HandleDefaultState()
         {
-            if (!other.CompareTag("Enemy"))
-                return;
-
-            var enemy = other.GetComponent<EnemyHitBox>().Enemy;
-            HandleEnemyEnteredRange(enemy);
+            _turretRotation.RotateToInitialPosition();
+            _turretGun.TryReloading();
         }
 
-        private void HandleEnemyEnteredRange(Enemy enemy)
-        {
-            enemy.onDestroyCallback += StopTrackingIfDestroyed;
-            _enemiesInRange.Add(enemy);
-
-            if (_enemiesInRange.Count == 1)
-                LockedOnEnemy = enemy;
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            if (!other.CompareTag("Enemy"))
-                return;
-
-            var enemy = other.GetComponent<EnemyHitBox>().Enemy;
-            HandleEnemyExitedRange(enemy);
-        }
-
-        private void HandleEnemyExitedRange(Enemy enemy)
-        {
-            _enemiesInRange.Remove(enemy);
-
-            if (HasEnemiesInRange)
-                LockedOnEnemy = _enemiesInRange[0]; // grad the first available enemy in range
-        }
-
-        private void StopTrackingIfDestroyed(Enemy enemy)
-        {
-            if (!_enemiesInRange.Contains(enemy))
-                return;
-
-            _enemiesInRange.Remove(enemy);
-            if (HasEnemiesInRange)
-                LockedOnEnemy = _enemiesInRange[0]; // grad the first available enemy in range
-        }
-
-
-        public Enemy LockedOnEnemy { get; private set; }
+        public Enemy LockedOnEnemy => _turretLockOn.LockedOnEnemy;
         public Vector3 DirectionToLockedOnEnemy => _turretRotation.DirectionToTarget;
         public bool IsLookingAtEnemy => _turretRotation.IsLookingAtTarget;
-        public bool HasEnemiesInRange => _enemiesInRange.Count != 0;
+        public bool HasEnemiesInRange => _turretLockOn.HasEnemiesInRange;
         public Transform RotationObject => _turretRotation.rotationObj;
 
     }
