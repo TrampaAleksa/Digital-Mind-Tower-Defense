d69e270c7e9fdbd71e6ed5785537dec7035c9f1d
Aleksa Trampa <alektramp@gmail.com>
2023-04-02 10:40:02 +0200

    Debugged and fixed bugs with auto turret lock on detection

--- Assets/_Project/Scripts/AutomatedTurrets/AutoTurret.cs
+++ Assets/_Project/Scripts/AutomatedTurrets/AutoTurret.cs
@@ -10,9 +10,10 @@ namespace com.digitalmind.towertest
     {
         public GameObject projectile;
         public Transform rotationObj;
-        
+
         public float range = 1.8f;
         public float rotSpeed = 1f;
+        public float lockOnAngle = 2.5f; 
 
         private List<Enemy> _enemiesInRange = new List<Enemy>();
         private Enemy _lockedOnEnemy;
@@ -30,11 +31,22 @@ namespace com.digitalmind.towertest
             {
                 Quaternion lookOnLook =
                     Quaternion.LookRotation(_lockedOnEnemy.transform.position - rotationObj.position);
-            
+
                 rotationObj.rotation =
                     Quaternion.Slerp(rotationObj.rotation, lookOnLook, Time.deltaTime * rotSpeed);
+
+                float angle = 2.5f;
+                
+                var lookingAtEnemy = Vector3.Angle(rotationObj.forward,
+                    _lockedOnEnemy.transform.position - rotationObj.position) < angle;
+
+                if (lookingAtEnemy)
+                {
+                    Debug.Log("Locked on and ready to shoot!");
+                }
+
             }
-            
+
             if (_enemiesInRange.Count == 0)
             {
                 rotationObj.rotation =
@@ -71,10 +83,38 @@ namespace com.digitalmind.towertest
         {
             if (!_enemiesInRange.Contains(enemy))
                 return;
-            
+
             _enemiesInRange.Remove(enemy);
             if (_enemiesInRange.Count != 0)
                 _lockedOnEnemy = _enemiesInRange[0]; // grad the first available enemy in range
+        }
+
+
+        
+        private void DebugTurret()
+        {
+            
+            var debugAngle = false;
+            var debugLooking = false;
+            
+            Debug.DrawRay(rotationObj.position, rotationObj.forward*50f, Color.red);
+            
+            if (_enemiesInRange.Count != 0)
+            {
+                Debug.DrawRay(rotationObj.position, _lockedOnEnemy.transform.position);
+                
+                if (debugAngle)
+                    Debug.Log(Vector3.Angle(rotationObj.forward,
+                        _lockedOnEnemy.transform.position - rotationObj.position));
+                
+                if (debugLooking)
+                {
+                    var lookingAtEnemy = Vector3.Angle(rotationObj.forward,
+                        _lockedOnEnemy.transform.position - rotationObj.position) < lockOnAngle;
+                    
+                    Debug.Log("Looking: " + lookingAtEnemy);
+                }
+            }
         }
     }
 }
