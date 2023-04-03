12d738f13031cd64754de95abfe1595d98ea671d
Aleksa Trampa <alektramp@gmail.com>
2023-04-03 14:21:23 +0200

    Added script that handles camera viewport for mobile devices

--- Assets/_Project/Scripts/UI/PauseUI.cs
+++ Assets/_Project/Scripts/UI/PauseUI.cs
@@ -9,11 +9,13 @@ namespace com.digitalmind.towertest
     public class PauseUI : MonoBehaviour
     {
         public GameObject gameUIPanel;
+        public GameObject inputUIPanel;
         public PauseGameHandler pauseGameHandler;
         
         public void DisplayPausePanel()
         {
             gameUIPanel.SetActive(false);
+            inputUIPanel.SetActive(false);
             gameObject.SetActive(true);
             pauseGameHandler.SetIsPaused(true);
         }
@@ -21,6 +23,7 @@ namespace com.digitalmind.towertest
         public void ContinueGame()
         {
             gameUIPanel.SetActive(true);
+            inputUIPanel.SetActive(true);
             gameObject.SetActive(false);
             pauseGameHandler.SetIsPaused(false);
         }
