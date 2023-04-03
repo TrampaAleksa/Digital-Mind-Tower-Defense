233c3298a43982782e060830c4b0264fd655f7fe
Aleksa Trampa <alektramp@gmail.com>
2023-04-03 15:04:42 +0200

    Used raycast utility for build location detection

--- Assets/_Project/Scripts/AutomatedTurrets/AutoTurretBuildClickDetection.cs
+++ Assets/_Project/Scripts/AutomatedTurrets/AutoTurretBuildClickDetection.cs
@@ -15,34 +15,10 @@ namespace com.digitalmind.towertest
 
         public bool ClickedOnBuildLocation()
         {
-            if (Application.platform == RuntimePlatform.Android)
-                return HasTouchedBuildLocationAndroid();
-            
-            if (!Input.GetKeyDown(KeyCode.Mouse0))
-                return false;
-
-            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-            var clickedOnBuildLocation = Physics.Raycast(ray, out _hit, 100f, LayerMask.GetMask("BuildLocation"));
-
-            return clickedOnBuildLocation;
+            return RaycastUtility.CheckIfClicked(out _hit, _camera, "BuildLocation");
         }
 
         public BuildLocation BuildLocationHit => _hit.collider
             .GetComponent<BuildLocation>();
-
-        
-        private bool HasTouchedBuildLocationAndroid()
-        {
-            for (var i = 0; i < Input.touchCount; ++i)
-            {
-                if (Input.GetTouch(i).phase != TouchPhase.Began) continue;
-
-                Ray ray = _camera.ScreenPointToRay(Input.GetTouch(i).position);
-                if (Physics.Raycast(ray, out _hit,100f, LayerMask.GetMask("BuildLocation")))
-                    return true;
-            }
-
-            return false;
-        }
     }
 }
