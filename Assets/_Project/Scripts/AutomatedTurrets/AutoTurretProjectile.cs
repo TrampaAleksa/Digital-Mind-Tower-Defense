ee8e233066b026fe94a714e8d6ea876cc5a19855
Aleksa Trampa <alektramp@gmail.com>
2023-04-03 13:45:53 +0200

    fixed bug with auto turret projectiles null reference

--- Assets/_Project/Scripts/AutomatedTurrets/AutoTurretProjectile.cs
+++ Assets/_Project/Scripts/AutomatedTurrets/AutoTurretProjectile.cs
@@ -6,16 +6,34 @@ namespace com.digitalmind.towertest
 {
     public class AutoTurretProjectile : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
+        public float speed = 120f;
+        public float damage = 1f;
         void Update()
         {
-        
+            transform.Translate(transform.right * (speed * Time.deltaTime), Space.World);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                DamageEnemy(other.GetComponent<EnemyHitBox>().Enemy);
+                Despawn();
+                return;
+            }
+
+            if (other.CompareTag("Boundary"))
+                Despawn();
+        }
+
+        private void DamageEnemy(Enemy enemy)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        private void Despawn()
+        {
+            Destroy(gameObject);
         }
     }
 }
